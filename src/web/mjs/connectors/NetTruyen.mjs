8052f95ac0d7d7aaf7a40f9ae5c2add69234e153
yutakatoshiro <minhhnbcdz2005@gmail.com>
2025-04-02 10:42:16 +0700

    Update NetTruyen.mjs
    Because this change has been over 2 years, so I changed to a new domain. Thank you

--- src/web/mjs/connectors/NetTruyen.mjs
+++ src/web/mjs/connectors/NetTruyen.mjs
@@ -12,7 +12,7 @@ export default class NetTruyen extends MojoPortalComic {
                 label: 'URL',
                 description: 'This website changes their URL regularly.\nThis is the last known URL which can also be manually set by the user.',
                 input: 'text',
-                value: 'https://www.nettruyenus.com'
+                value: 'https://nettruyenvie.com/'
             }
         };
         this.links = {
